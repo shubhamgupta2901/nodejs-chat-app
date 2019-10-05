2613e62e19e617c4b58a48ad0fb91443f9026254
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-10-06 00:55:17 +0530

    displaying messages and locations in front end

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,33 +1,56 @@
 
 const socket = io();  
 
-const form = document.querySelector('form');
-const messageInput = document.querySelector('#message');
-const sendLocationButton = document.querySelector('#location');
+// Elements
+const $form = document.querySelector('#message-form');
+const $messageFormInput = $form.querySelector('input');
+const $messageFormButton = $form.querySelector('button');
+const $sendLocationButton = document.querySelector('#location');
+const $messageContainer = document.querySelector('#messages');
 
-form.addEventListener('submit',(event)=>{
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+socket.on('message',message=>{
+    console.log(message);
+    const html = Mustache.render(messageTemplate,{message});
+    $messageContainer.insertAdjacentHTML('beforeend',html);
+});
+
+socket.on('locationMessage', (locationMessage)=>{
+    console.log(locationMessage);
+    const html = Mustache.render(locationMessageTemplate,{
+        location: locationMessage
+    });
+    $messageContainer.insertAdjacentHTML('beforeend',html);
+});
+
+$form.addEventListener('submit',(event)=>{
     event.preventDefault(); 
-    const message = messageInput.value;
-    if(message){
-        socket.emit('sendMessage',message,(callbackMessage)=>{
-                console.log(callbackMessage);
-        });
-    }
+    //disable button
+    $messageFormButton.setAttribute('disabled','disabled');
+    const message = $messageFormInput.value;
+    socket.emit('sendMessage',message,(callbackMessage)=>{
+        //enable button again
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        console.log(callbackMessage);
+    });
+    
 })
 
-sendLocationButton.addEventListener('click', (event)=>{
+$sendLocationButton.addEventListener('click', (event)=>{
     if(!navigator.geolocation){
         return alert('Location Services not available');
     }
+    $sendLocationButton.setAttribute('disabled','disabled');
     navigator.geolocation.getCurrentPosition((position) =>{
         const {latitude, longitude} = position.coords;
-        console.log(latitude, longitude);
         socket.emit('sendLocation',{latitude, longitude},(callbackMessage)=>{
-            console.log('Location shared!');
+            console.log(callbackMessage);
+            $sendLocationButton.removeAttribute('disabled');
         })
      });
 })
 
-socket.on('message',message=>{
-    console.log(message);
-});
