50485c31983b72dffcbf86ee30eb7ad50195d421
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-10-05 14:52:45 +0530

    setting up http server to support websockets

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
-const express = require('express');
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
 
+/**
+ * Changing the way we create a web server with express.
+ * This work of creating http server is done by express under the hood, we are it explicitly to use socket.io as well
+ * Socket.io expects to be called with raw http server. When express creates it behind the scene we don't have access to it 
+ * to pass it in socketio constructor.
+ * Now our http server supports web sockets too.
+ * 
+ */
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 const port = 3000;
 
 const publicDirectoryPath = path.join(__dirname,'../public');
 app.use(express.static(publicDirectoryPath));
 
-// app.get('',(request,response)=>{
-//     response.sendFile(path.join(publicDirectoryPath+'/index.html'));
-// })
+io.on('connection',()=>{
+    console.log('New web socket connection');
+})
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 })
